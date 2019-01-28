ea5d634e0beab4e9ca8edb474fe6b81e43bc6c2e
Viktor Petersson <vpetersson@wireload.net>
2019-01-28 20:06:43 +0000

    Fixes bug that turned strings into tuples

--- backend/device_registry/api_views.py
+++ backend/device_registry/api_views.py
@@ -217,9 +217,9 @@ def mtls_ping_view(request, format=None):
     elif request.method == 'POST':
         device_info_object = DeviceInfo.objects.get(device__device_id=device_id)
         device_info_object.device__last_ping = timezone.now()
-        device_info_object.device_operating_system_version = request.data.get('device_operating_system_version'),
-        device_info_object.fqdn = request.data.get('fqdn'),
-        device_info_object.ipv4_address = request.data.get('ipv4_address'),
+        device_info_object.device_operating_system_version = request.data.get('device_operating_system_version')
+        device_info_object.fqdn = request.data.get('fqdn')
+        device_info_object.ipv4_address = request.data.get('ipv4_address')
         device_info_object.save()
     else:
         return Response({
@@ -300,11 +300,11 @@ def mtls_renew_cert_view(request, format=None):
     device_info_object, created = DeviceInfo.objects.update_or_create(device=device_object)
     device_info_object.device_manufacturer = request.data.get('device_manufacturer')
     device_info_object.device_model = request.data.get('device_model'),
-    device_info_object.device_operating_system = request.data.get('device_operating_system'),
-    device_info_object.device_operating_system_version = request.data.get('device_operating_system_version'),
-    device_info_object.device_architecture = request.data.get('device_architecture'),
-    device_info_object.fqdn = request.data.get('fqdn'),
-    device_info_object.ipv4_address = request.data.get('ipv4_address'),
+    device_info_object.device_operating_system = request.data.get('device_operating_system')
+    device_info_object.device_operating_system_version = request.data.get('device_operating_system_version')
+    device_info_object.device_architecture = request.data.get('device_architecture')
+    device_info_object.fqdn = request.data.get('fqdn')
+    device_info_object.ipv4_address = request.data.get('ipv4_address')
     device_info_object.save()
 
     return Response({
