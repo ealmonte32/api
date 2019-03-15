1478ae898a8bc2122bceff5a3b4bd333cf7ebdb2
Viktor Petersson <vpetersson@wireload.net>
2019-03-15 08:27:35 +0000

    Refactors local development environment
    * Introduces Postgres as the database
    * Automatically runs migrations on start

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -4,11 +4,17 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'wott-backend'),
+        'USER': os.getenv('DB_USER', 'wott-backend'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'psql'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'connect_timeout': 3,
+        },
     }
 }
-
 COMMON_NAME_PREFIX = 'd.wott-dev.local'
 STATIC_URL = 'http://localhost:8003/'
 INSTALLED_APPS += ['django_extensions']
