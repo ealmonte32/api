f80ad0c9a8f34211f9580f6b48f6a9a1234920f5
eugenosm <emazitov@gmail.com>
2019-06-13 14:06:32 +0600

    minor changes

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', "0")))
 
-ALLOWED_HOSTS = ['localhost','192.168.1.6', '192.168.1.17', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
