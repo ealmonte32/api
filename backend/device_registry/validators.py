bea8c1958aced3ffc70e02fbfc340b84c7606844
eugenosm <emazitov@gmail.com>
2019-06-20 18:48:49 +0600

    change credential name validator message

--- backend/device_registry/validators.py
+++ backend/device_registry/validators.py
@@ -8,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 class UnicodeNameValidator(validators.RegexValidator):
     regex = r'^[\w.@+-:]+$'
     message = _(
-        'Enter a valid username. This value may contain only letters, '
+        'Enter a valid name. This value may contain only letters, '
         'numbers, and @/./+/-/_/: characters.'
     )
     flags = re.UNICODE
