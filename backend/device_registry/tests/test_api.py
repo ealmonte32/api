22df17f84aa253010ce2b5f8b167d04ecf02dfcd
eugenosm <emazitov@gmail.com>
2019-07-26 02:34:48 +0600

    device enroll test due to error message changed

--- backend/device_registry/tests/test_api.py
+++ backend/device_registry/tests/test_api.py
@@ -877,7 +877,7 @@ class DeviceEnrollView(APITestCase):
         response = self.client.post(self.url, data=payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         error_data = {
-            'non_field_errors': [ErrorDetail(string='Claim-token for this device not found', code='invalid')]
+            'non_field_errors': [ErrorDetail(string='Device id and claim token do not match', code='invalid')]
         }
         self.assertEqual(response.data, error_data)
         self.assertTrue(PairingKey.objects.filter(key=self.pairing_key.key).exists())
