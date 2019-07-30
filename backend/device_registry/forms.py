dd0343740e3e6f2677856dd597144c565e5de217
Artem Martynovich <artem.martynovich@gmail.com>
2019-07-31 00:47:46 +0600

    Remove commented code.

--- backend/device_registry/forms.py
+++ backend/device_registry/forms.py
@@ -3,24 +3,9 @@ from django import forms
 import tagulous.forms
 
 from .models import Device, FirewallState, DeviceInfo
-# from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
 from bootstrap_datepicker_plus import DateTimePickerInput
 
-#
-#
-# class FilterForm(forms.Form):
-#     datetime_field = forms.DateTimeField(
-#         widget=DateTimePicker(
-#             options={
-#                 'useCurrent': True,
-#                 'collapse': False,
-#             },
-#             attrs={
-#                 'append': 'fa fa-calendar',
-#                 'icon_toggle': True,
-#             }
-#         ),
-#     )
+
 class FilterForm(forms.Form):
     datetime_field = forms.DateTimeField(
          widget=DateTimePickerInput()
