9c10502c52dcc67b2a0824f3ec113a5fbf13bae1
YRM <y.mkhhlv@yahoo.com>
2019-08-12 13:49:40 +0600

    webpack config upd

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,28 +1,49 @@
-var path = require("path")
-var webpack = require('webpack')
-var BundleTracker = require('webpack-bundle-tracker')
+const Webpack = require('webpack');
+const Path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry:[ 'core-js','jquery','jquery-validation','classnames','simplebar','popper.js','daterangepicker','fullcalendar'],
-
-  output: {
-      path: path.resolve('./backend/static/bundles/'),
-      filename: "[name]-[hash].js",
+  entry: {
+    app: './static/js/app'
   },
-
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'inline-source-map',
+  output: {path: path.resolve('./backend/static/bundles/'),filename:"[name]-[hash].js"},
+  performance: { hints: false },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
+    new MiniCssExtractPlugin({filename: 'css/[name].css',chunkFilename: 'css/[id].css'}),
+    new Webpack.ProvidePlugin({$: 'jquery',jQuery: 'jquery','window.$': 'jquery','window.jQuery': 'jquery',Popper: ['popper.js', 'default']}),
+    new HardSourceWebpackPlugin(),
+	new BundleTracker({filename: './webpack-stats.json'})
   ],
-
   module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}, 
-    ],
+    rules: [
+      {test: /\.js$/,exclude: /(node_modules)/,loader: ['babel-loader']},
+      {test: /\.scss$/,use: [MiniCssExtractPlugin.loader,'css-loader','postcss-loader','sass-loader']},
+      {test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,loader : 'url-loader',options: {limit: 100000}},
+      {test   : /\.(png|jpg|jpeg|gif?)(\?[a-z0-9=&.]+)?$/,loader : 'url-loader',options: {limit: 100000}},
+      {test: require.resolve('jquery'),use: [{loader: 'expose-loader',options: 'jQuery'},{loader: 'expose-loader',options: '$'}]}
+    ]
   },
-
   resolve: {
-    modulesDirectories: ['node_modules', 'bower_components'],
-    extensions: ['', '.js', '.jsx']
-  },
-} 
+    extensions: ['.js', '.scss'],
+    modules: ['node_modules'],
+    alias: {request$: 'xhr'}
+  }
+}
