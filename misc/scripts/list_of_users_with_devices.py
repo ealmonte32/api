d357996c76fad12eff86e3c9cec35751d0d2de7f
Viktor Petersson <vpetersson@wireload.net>
2019-08-15 13:21:38 +0100

    Improves parsing of user list

--- misc/scripts/list_of_users_with_devices.py
+++ misc/scripts/list_of_users_with_devices.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db.models import Count
 
-print(list(User.objects.exclude(
-    devices__isnull=True).annotate(num_devices=Count('devices')).values_list('username', 'email', 'num_devices')))
+print('user,email,device_count')
+for user in User.objects.exclude(
+        devices__isnull=True).annotate(
+                num_devices=Count('devices')):
+    print('{},{},{}'.format(user.username, user.email, user.num_devices))
