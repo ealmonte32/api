1db6d4366c74d05660745de4ca869b4a1ede7c7f
Artem Martynovich <artem.martynovich@gmail.com>
2019-08-22 15:03:01 +0600

    Add clipboard-polyfill, use it for copying "Add Device" code. Remove commented code in app.js.

--- backend/backend/static/js/app.js
+++ backend/backend/static/js/app.js
@@ -3,37 +3,33 @@ import "../scss/app.scss";
 
 // Global
 import "./modules/bootstrap";
-// import "./modules/dragula";
 import "./modules/feather";
 import "./modules/font-awesome";
 import "./modules/moment";
 import "./modules/sidebar";
-import "./modules/toastr";
 import "./modules/user-agent";
 
 // Charts
 import "./modules/chartjs";
-// import "./modules/apexcharts";
 
 // Forms
-// import "./modules/daterangepicker";
-// import "./modules/datetimepicker";
-// import "./modules/fullcalendar";
-// import "./modules/markdown";
 import "./modules/mask";
-// import "./modules/quill";
 import "./modules/select2";
 import "./modules/validation";
-// import "./modules/wizard";
-
-// Maps
-// import "./modules/vector-maps";
 
 // Tables
 import "./modules/datatables";
 
+// Code highlight
 import hljs from 'highlight.js';
 window.hljs = hljs;
 
+// Tags
 window.Tagulous = require('exports-loader?Tagulous!../../staticfiles/tagulous/tagulous.js')
 require('exports-loader?window.select23!../../staticfiles/tagulous/adaptor/select2-3.js')
+
+// Copy to clipboard
+import * as clipboard from "clipboard-polyfill"
+window.copyToClipboard = function(text) {
+    return clipboard.writeText(text).then(console.log, console.error);
+}
