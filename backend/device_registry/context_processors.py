a6a1818eda7f6c02616906398323ac84df092304
Artem Martynovich <artem.martynovich@gmail.com>
2019-10-07 15:29:44 +0600

    Remove extra space in device_info_tabs. Use django.conf.settings instead of backend.settings.

--- backend/device_registry/context_processors.py
+++ backend/device_registry/context_processors.py
@@ -1,11 +1,11 @@
 import json
 from pathlib import Path
-from backend.settings.base import BASE_DIR, MIXPANEL_TOKEN
+from django.conf import settings
 
 WEBPACK_STATS_NAME = 'webpack-stats.json'
 webpack_stats_path = Path('/usr/src/misc') / WEBPACK_STATS_NAME
 if not webpack_stats_path.is_file():
-    webpack_stats_path = Path(BASE_DIR) / '..' / WEBPACK_STATS_NAME
+    webpack_stats_path = Path(settings.BASE_DIR) / '..' / WEBPACK_STATS_NAME
 with webpack_stats_path.open() as webpack_stats_file:
     webpack_bundle_json = json.load(webpack_stats_file)
     WEBPACK_BUNDLE_CSS, WEBPACK_BUNDLE_JS = ['/bundles/'+chunk['name'] for chunk in webpack_bundle_json['chunks']['app']]
@@ -17,5 +17,5 @@ def webpack_bundle(request):
             'js': WEBPACK_BUNDLE_JS,
             'css': WEBPACK_BUNDLE_CSS
         },
-        'MIXPANEL_TOKEN': MIXPANEL_TOKEN
+        'MIXPANEL_TOKEN': settings.MIXPANEL_TOKEN
     }
