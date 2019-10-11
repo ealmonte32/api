7de784f0bb4cc161c698eca1b814dce9dd2e4d20
Artem Martynovich <artem.martynovich@gmail.com>
2019-10-11 21:04:19 +0600

    Add trailing slash to profile/wizard-complete.

--- backend/profile_page/urls.py
+++ backend/profile_page/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('profile/', ProfileAccountView.as_view(), name='profile'),
     path('profile/password/', PasswordChangeView.as_view(
         success_url=reverse_lazy('profile_password'), template_name='profile_password.html'), name='profile_password'),
-    path('profile/wizard-complete', WizardCompleteView.as_view(), name='wizard-complete'),
+    path('profile/wizard-complete/', WizardCompleteView.as_view(), name='wizard-complete'),
     path('profile/token/', ProfileAPITokenView.as_view(), name='profile_token'),
     path('generate-api-token/', GenerateAPITokenView.as_view(), name='generate_api_token'),
     path('revoke-api-token/', RevokeAPITokenView.as_view(), name='revoke_api_token')
