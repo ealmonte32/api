59ffc5229f46d2de26865e418cdf27807f88dbce
Artem Martynovich <artem.martynovich@gmail.com>
2019-10-18 00:02:47 +0600

    Workaround for Fontawesome to reduce bundle size.

--- backend/backend/static/js/modules/font-awesome.js
+++ backend/backend/static/js/modules/font-awesome.js
@@ -1,11 +1,21 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import {fas,
-    faCopy, faCheck, faQuestionCircle, faExclamationCircle, faEdit,
-    faSave, faEye, faInfoCircle
-} from '@fortawesome/free-solid-svg-icons'
-import {far, faSquare, faCheckSquare} from '@fortawesome/free-regular-svg-icons'
 
-library.add(far, [faSquare, faCheckSquare]);
-library.add(fas, [faCopy, faCheck, faQuestionCircle, faExclamationCircle, faEdit,
-    faSave, faEye, faInfoCircle]);
+/*
+ Webpack fails to properly tree-shake Fontawesome's index.es.js, see
+ https://github.com/FortAwesome/Font-Awesome/issues/14552
+ This workaround helps reduce bundle size by 600+ KiB,
+ */
+import { faSquare } from '@fortawesome/free-regular-svg-icons/faSquare'
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons/faCheckSquare'
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy'
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons/faQuestionCircle'
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle'
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit'
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave'
+import { faEye } from '@fortawesome/free-solid-svg-icons/faEye'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle'
+
+
+library.add([faCopy, faCheck, faQuestionCircle, faExclamationCircle, faEdit, faSquare, faCheckSquare, faSave, faEye, faInfoCircle]);
 dom.watch();
