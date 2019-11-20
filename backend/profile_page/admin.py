4786ebaccf044bc64fc7344a7cdb2e26f1f976d3
Roman P <pythonpro@gmail.com>
2019-11-20 18:31:47 +0400

    Add ability to sort users by node count

--- backend/profile_page/admin.py
+++ backend/profile_page/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.db.models import Count
 
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined', 'last_login',
-                    'nodes_count')
+                    'node_count')
 
-    def nodes_count(self, obj):
-        return obj.devices.count()
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(_node_count=Count('devices'))
+        return qs
+
+    def node_count(self, obj):
+        return obj._node_count
+
+    node_count.admin_order_field = '_node_count'
 
 
 admin.site.unregister(User)
