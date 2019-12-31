bb306ecc9cbc32a72f05c055a19f31722eac217e
Artem Martynovich <artem.martynovich@gmail.com>
2019-12-31 19:58:21 +0600

    Update Fontawesome to 5.12.0. It has https://github.com/FortAwesome/Font-Awesome/issues/14552 resolved. Also update everything else.

--- backend/backend/static/js/modules/font-awesome.js
+++ backend/backend/static/js/modules/font-awesome.js
@@ -1,22 +1,8 @@
 import {dom, library} from '@fortawesome/fontawesome-svg-core'
-/*
- Webpack fails to properly tree-shake Fontawesome's index.es.js, see
- https://github.com/FortAwesome/Font-Awesome/issues/14552
- This workaround helps reduce bundle size by 600+ KiB,
- */
-import {faBan} from '@fortawesome/free-solid-svg-icons/faBan'
-import {faCheckCircle} from '@fortawesome/free-solid-svg-icons/faCheckCircle'
-import {faSquare} from '@fortawesome/free-regular-svg-icons/faSquare'
-import {faCheckSquare} from '@fortawesome/free-regular-svg-icons/faCheckSquare'
-import {faCopy} from '@fortawesome/free-solid-svg-icons/faCopy'
-import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck'
-import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub'
-import {faQuestionCircle} from '@fortawesome/free-solid-svg-icons/faQuestionCircle'
-import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons/faExclamationCircle'
-import {faEdit} from '@fortawesome/free-solid-svg-icons/faEdit'
-import {faSave} from '@fortawesome/free-solid-svg-icons/faSave'
-import {faEye} from '@fortawesome/free-solid-svg-icons/faEye'
-import {faInfoCircle} from '@fortawesome/free-solid-svg-icons/faInfoCircle'
+
+import {faBan, faCheckCircle, faCopy, faCheck, faQuestionCircle, faExclamationCircle, faEdit, faSave, faEye, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {faSquare, faCheckSquare} from '@fortawesome/free-regular-svg-icons'
 
 
 library.add([
