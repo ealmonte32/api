dd17ca963a30df2c07be35efc296be8636625623
Artem Martynovich <artem.martynovich@gmail.com>
2020-01-22 18:16:07 +0600

    Template for sidebar item.

--- backend/device_registry/urls.py
+++ backend/device_registry/urls.py
@@ -58,6 +58,7 @@ if settings.IS_DASH or settings.IS_CELERY:
         path('',
              views.RootView.as_view(),
              name='root'),
+        path('', views.RootView.as_view(), name='nodes'),
         path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
         path('policies/', views.GlobalPoliciesListView.as_view(), name='global_policies'),
         path('policies/add/', views.GlobalPolicyCreateView.as_view(), name='create_global_policy'),
