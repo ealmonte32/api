5048600d7bc494631ea9bb2eacca2dd22844537a
Artem Martynovich <artem.martynovich@gmail.com>
2020-02-11 11:28:31 +0600

    Ensure FirewallDisabledAction is generated after firewall setting change.

--- backend/device_registry/views.py
+++ backend/device_registry/views.py
@@ -415,6 +415,7 @@ class DeviceDetailSecurityView(LoginRequiredMixin, LoginTrackMixin, DetailView):
                 portscan.block_networks = out_data
                 portscan.save(update_fields=['block_networks'])
 
+        self.object.refresh_from_db()
         self.object.generate_recommended_actions(classes=[FirewallDisabledAction])
         return HttpResponseRedirect(reverse('device-detail-security', kwargs={'pk': kwargs['pk']}))
 
